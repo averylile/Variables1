f3189a75118c409b74590c4130efc6b8749d0e70
Avery Lile <lileave000@warren.k12.in.us>
2024-09-27 09:12:25 -0400

    initial commit 2nd commit

--- Variables.py
+++ Variables.py
@@ -1,6 +1,7 @@
-num_apples = 20
-num_oranges = 15
-print( "Number of apples:"):"number of oranges"
-#instr 01: write code to print the oranges similar to line 2
-#Instr 02: change the num_oranges t0 0 like I did in line 2.
-#Instr 04: print oranges again like i did in line 2
+Number_of_apples = 20
+print("Number of apples:", Number_of_apples)
+Number_of_oranges = 15
+print("Number of oranges:", Number_of_oranges)
+print("Number of apples:", Number_of_apples)
+Number_of_oranges = 0
+print("Number of oranges:", Number_of_oranges)
